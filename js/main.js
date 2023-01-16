114bf24187d34fced9a426015f947a0acf9836db
heayriwang <118506446+heayriwang@users.noreply.github.com>
2023-01-16 17:43:12 +0900

    23.01.16

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $('#header .search_toggle button').on('click', function () {
+        // $('.header .search').slideToggle();
+        $('#header .search').toggleClass('on');
+    })
+
+
+})
